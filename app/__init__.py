b3670d608918516d6049d777f7f08b60ae7e0001
beljjvalentin <153101222+LCF2345@users.noreply.github.com>
2024-10-09 18:03:48 -0400

    Fixing Mongo Credentials

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,8 @@ flask_app.static_folder = 'static'
 
 # MongoDB Atlas Connection
 client = MongoClient("mongodb+srv://valentin:dLnI23EfvYpg3pua@cluster0.flk7i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-db = client.get_database("realize-circuits")  # Replace "app" with your database name
+db = client.get_database("shop_db")  # Replace "app" with your database name
 
-products_collection = db.MyProducts  # Replace products with your collection name
+products_collection = db.products  # Replace products with your collection name
 
 from app import routes
--- insert_data.py
+++ insert_data.py
@@ -4,8 +4,8 @@ from pymongo import MongoClient
 # MongoDB Atlas Connection
 client = MongoClient("mongodb+srv://valentin:dLnI23EfvYpg3pua@cluster0.flk7i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 #db = client.app  # Replace "app" with your database name
-db = client.get_database("realize-circuits")  # Replace products with your collection name
-products_collection = db.MyProducts
+db = client.get_database("shop_db")  # Replace products with your collection name
+products_collection = db.products
 
 # Example product objects
 products = [
